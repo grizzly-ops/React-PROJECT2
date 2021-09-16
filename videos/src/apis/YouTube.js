b31f722512f6ff8760c2a1652e374297374b4af2
ndabe <stonerlife420k@gmail.com>
2021-09-16 13:52:10 +0200

    React

--- videos/src/apis/YouTube.js
+++ videos/src/apis/YouTube.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyBTJw74IiVpJ5xSerGrtg_Kw2pC7zARs1g'
+const KEY = 'AIzaSyDLnKycei0QP74RnRURjFsXvP8XQD82gQQ';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
     params: {
         part: 'snippet',
         maxResults: 5,
-        key: KEY   
+        key: KEY,
     }
 });
