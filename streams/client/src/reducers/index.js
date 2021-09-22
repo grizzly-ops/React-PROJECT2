75b1f0fe3c92d3e2bc45625bd3a6d213a429323e
ndabe <stonerlife420k@gmail.com>
2021-09-22 10:05:18 +0200

    React

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
+import StreamReducer from './StreamReducer';
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer,
+  streams: StreamReducer
 });
